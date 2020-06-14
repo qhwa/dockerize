16710778c095c47d2aef71088d7f68a2181aefa7
qhwa <qhwa@163.com>
2020-06-14 18:06:02 +0800

    bump version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Dockerize.MixProject do
       app: :dockerize,
       description: "A tool for creating docker image from an Elixir project.",
       license: "",
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
