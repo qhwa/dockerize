c8f03a0458e5f035c5986071a7d2a2c8c398c17c
qhwa <qhwa@163.com>
2020-06-15 09:22:19 +0800

    pass dialyzing

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Dockerize.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :mix, :eex]
     ]
   end
 
