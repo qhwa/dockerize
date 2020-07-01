00f976f43c581f839f530830d4972e4e41f29b46
qhwa <qhwa@163.com>
2020-07-01 08:41:30 +0800

    remove unexpected files

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Dockerize.MixProject do
       app: :dockerize,
       description: "A tool for creating docker image from an Elixir project.",
       license: "",
-      version: "0.2.0-alpha.2",
+      version: "0.2.0-alpha.3",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
--- test_projects/simple_app/README.md
+++
@@ -1,1 +0,0 @@
-This is a example project for testing dockerfile generating and image building in projects without phoenix.
