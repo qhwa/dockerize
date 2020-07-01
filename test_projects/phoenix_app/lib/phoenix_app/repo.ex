ed2648cae64604be050ae08d5f1dbf570ab9a822
qhwa <qhwa@163.com>
2020-07-01 08:41:30 +0800

    remove unexpected files

--- test_projects/phoenix_app/lib/phoenix_app/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule PhoenixApp.Repo do
-  use Ecto.Repo,
-    otp_app: :phoenix_app,
-    adapter: Ecto.Adapters.Postgres
-end
--- test_projects/phoenix_app/lib/phoenix_app_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule PhoenixAppWeb.PageController do
-  use PhoenixAppWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
